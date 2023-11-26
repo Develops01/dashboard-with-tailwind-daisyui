86086f40f2f4cb21801d1b76862eb847ecea791b
mohammad <ronins1375@gmail.com>
2023-11-26 20:59:46 +0330

    nonResponsive

---
+++ app.js
@@ -0,0 +1,12 @@
+let darkmode = document.querySelector("[data-theme]");
+let val = darkmode.getAttribute("data-theme")
+let num = 1
+const tog = document.getElementById("darkness");
+tog.addEventListener('click', () => {
+  num == 2 ? num = 2 : num = 1
+  num == 1 ? darkmode.setAttribute("data-theme", "light") : darkmode.setAttribute("data-theme", "dark")
+  num++
+})
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}'],
+  theme: ["light", "dark",],
+  plugins: [require("daisyui")],
+}
+
+
