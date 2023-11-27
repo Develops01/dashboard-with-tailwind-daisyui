8635a341e20abc19b999a8107e77cf3916efb615
mohammad <ronins1375@gmail.com>
2023-11-27 11:57:42 +0330

    responsive

--- app.js
+++ app.js
@@ -8,5 +8,11 @@ tog.addEventListener('click', () => {
   num++
 })
 
+// mobile menu
+const btn = document.querySelector(".mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
 
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
 
