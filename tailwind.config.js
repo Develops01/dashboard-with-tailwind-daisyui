f9251aee7a1526e2dfa017e61b8007c22a6d3461
mohammad <ronins1375@gmail.com>
2023-12-03 14:44:59 +0330

    fixing-bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}'],
-  theme: ["light", "dark",],
-  plugins: [require("daisyui")],
-  extend:
-  {
-    fontFamily: {
-      'Vazirmatn': ['Vazirmatn', 'sans-serif']
+  content: ['./*.{html,js}', './pages/*.{html,js}', './js/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Vazirmatn': ['Vazirmatn', 'sans-serif']
+      },
     },
-  }
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+
+
 }
 
